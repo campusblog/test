17b584572a74f71507c973d55e2fc8cd09dad58b
campusblog <1959321969@qq.com>
2018-05-23 12:51:31 +0800

    错误修改

--- src/main/java/com/java/controller/LoginController.java
+++ src/main/java/com/java/controller/LoginController.java
@@ -1,16 +1,12 @@
 package com.java.controller;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.DELETE;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.alibaba.fastjson.JSON;
 import com.java.pojo.Sys_role;
 import com.java.pojo.Sys_user;
 import com.java.service.impl.Sys_userServiceImpl;
